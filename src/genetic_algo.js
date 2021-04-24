726bff59ed0d5f04acf96e2db285704474c917e2
s-aki-2017 <s.akiyoshi.cel@gmail.com>
2021-04-24 15:00:57 +0900

    test push

---
+++ src/genetic_algo.js
@@ -0,0 +1,17 @@
+
+
+
+class GA {
+    constructor(Spot_N, Wantgo_M, Time_T, Satisfy_S){
+        this.Spot_N = Spot_N;
+        this.Wantgo_M = Wantgo_M;
+        this.Time_T = Time_T;
+        this.Satisfy_S = Satisfy_S;
+    }
+
+    
+
+    run(){
+        //
+    }
+}
